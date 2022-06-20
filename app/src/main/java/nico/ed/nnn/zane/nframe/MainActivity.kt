8e516728a3549d8fc895f32ca3019188ec42df0d
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-06-20 17:07:04 +0900

    セットアップ

---
+++ app/src/main/java/nico/ed/nnn/zane/nframe/MainActivity.kt
@@ -0,0 +1,43 @@
+package nico.ed.nnn.zane.nframe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nico.ed.nnn.zane.nframe.ui.theme.NFrameUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NFrameUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NFrameUITheme {
+        Greeting("Android")
+    }
+}
