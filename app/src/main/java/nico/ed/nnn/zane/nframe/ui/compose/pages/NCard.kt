9192bee34176e80f73bde7c28d2cef07b7c11574
Tatsuya Fujisaki <tatsuya.fujisaki.2022+github@gmail.com>
2022-06-28 15:59:33 +0900

    [KYOIKU-40625] NFrameUIアプリのNCard画面の作成 (3) (#13)

--- app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NCard.kt
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NCard.kt
@@ -1,10 +1,23 @@
 package nico.ed.nnn.zane.nframe.ui.compose.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nico.ed.nnn.zane.nframe.R
+import nico.ed.nnn.zane.nframe.data.NCardMedia
+import nico.ed.nnn.zane.nframe.data.PresentAbsent
 import nico.ed.nnn.zane.nframe.ui.compose.atoms.NFrameTopAppBar
+import nico.ed.nnn.zane.nframe.ui.compose.atoms.SelectionMenu
 
 @Preview
 @Composable
@@ -13,6 +26,79 @@ fun NCard() {
         topBar = {
             NFrameTopAppBar(R.string.n_card)
         }
-    ) {
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            Text(text = "設定項目", fontSize = 18.sp)
+
+            /**
+             * NCardMedia の設定
+             */
+            var nCardMediaSelected by remember { mutableStateOf(NCardMedia.IMAGE) }
+            Text(text = "NCardMedia", modifier = Modifier.padding(vertical = 12.dp))
+            SelectionMenu(
+                options = NCardMedia.values().toList(),
+                selectedValue = nCardMediaSelected.toString(),
+                selectOption = {
+                    nCardMediaSelected = it
+                }
+            )
+
+            /**
+             * アイコンの設定
+             */
+            var iconSelected by remember { mutableStateOf(PresentAbsent.PRESENT) }
+            Text(
+                text = "NCardHead - アイコン",
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+            SelectionMenu(
+                options = PresentAbsent.values().toList(),
+                selectedValue = iconSelected.toString(),
+                selectOption = { iconSelected = it }
+            )
+
+            /**
+             * タイトルの設定
+             */
+            var titleSelected by remember { mutableStateOf(PresentAbsent.PRESENT) }
+            var subtitleSelected by remember { mutableStateOf(PresentAbsent.PRESENT) }
+            Text(
+                text = "NCardHead - タイトル",
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+            SelectionMenu(
+                options = PresentAbsent.values().toList(),
+                selectedValue = titleSelected.toString(),
+                selectOption = {
+                    titleSelected = it
+                    // タイトルに「なし」を選択した場合は、サブタイトルも「なし」を選択する。
+                    if (it == PresentAbsent.ABSENT) subtitleSelected = PresentAbsent.ABSENT
+                }
+            )
+
+            /**
+             * サブタイトルの設定
+             */
+            Text(
+                text = "NCardHead - サブタイトル（タイトルなしの場合はなし）",
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+            SelectionMenu(
+                // タイトルに「なし」を選択した場合は、サブタイトルの選択肢も「なし」のみにする。
+                options = if (titleSelected == PresentAbsent.PRESENT) {
+                    PresentAbsent.values().toList()
+                } else {
+                    listOf(PresentAbsent.ABSENT)
+                },
+                selectedValue = subtitleSelected.toString(),
+                selectOption = {
+                    subtitleSelected = it
+                }
+            )
+        }
     }
 }
