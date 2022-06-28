024c796239fbd428c24996137f26e8bcd46dc41a
hiraike32 <takuya_hiraike@dwango.co.jp>
2022-06-28 11:50:44 +0900

    [KYOIKU-40637] NFrameUIアプリのNLayout画面の作成（1）

---
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/atoms/SelectionMenu.kt
@@ -0,0 +1,47 @@
+package nico.ed.nnn.zane.nframe.ui.compose.atoms
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SelectionMenu(
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    options: List<Any>,
+    selectedValue: String,
+    selectOption: (Any) -> Unit
+) {
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = onExpandedChange
+    ) {
+        TextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = { },
+            label = { Text("Label") },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { onExpandedChange(false) }
+        ) {
+            options.forEach { selectionOption ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectOption(selectionOption)
+                        onExpandedChange(false)
+                    }
+                ) {
+                    Text(text = selectionOption.toString())
+                }
+            }
+        }
+    }
+}
