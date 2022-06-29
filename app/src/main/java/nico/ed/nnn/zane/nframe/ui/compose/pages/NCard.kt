3758f486b1307d7ac042caea36090a76b2125277
Tatsuya Fujisaki <tatsuya.fujisaki.2022+github@gmail.com>
2022-06-29 12:56:55 +0900

    [KYOIKU-40683] NFrameUIアプリのNCard画面の作成 (5) (#18)

--- app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NCard.kt
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NCard.kt
@@ -1,7 +1,14 @@
 package nico.ed.nnn.zane.nframe.ui.compose.pages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
@@ -12,6 +19,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -146,6 +155,38 @@ fun NCard() {
                 selectedValue = nCardFootSelected.toString(),
                 selectOption = { nCardFootSelected = it }
             )
+
+            Text(
+                text = "表示確認",
+                fontSize = 18.sp,
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+
+            Box(
+                modifier = Modifier
+                    .width(316.dp)
+                    .height(160.dp)
+            ) {
+                when (nCardMediaSelected) {
+                    NCardMedia.IMAGE -> {
+                        Image(
+                            painter = painterResource(id = R.drawable.sample),
+                            modifier = Modifier.fillMaxSize(),
+                            contentDescription = null
+                        )
+                    }
+                    NCardMedia.SOLID -> {
+                        Spacer(
+                            modifier = Modifier
+                                .background(Color(0xff828282))
+                                .fillMaxSize()
+                        )
+                    }
+                    NCardMedia.MOVIE -> {
+                    }
+                    NCardMedia.NONE -> {}
+                }
+            }
         }
     }
 }
