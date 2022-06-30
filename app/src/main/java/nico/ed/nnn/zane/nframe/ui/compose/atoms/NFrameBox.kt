bd2189fa180fd0238007ab292cc9c67e04a720b1
Mithrandir815 <takayuki_iida@dwango.co.jp>
2022-06-30 17:39:36 +0900

    [KYOIKU-40709] NFrameUIアプリのNFrame画面の作成(3)

---
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/atoms/NFrameBox.kt
@@ -0,0 +1,78 @@
+package nico.ed.nnn.zane.nframe.ui.compose.atoms
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import nico.ed.nnn.zane.nframe.R
+import nico.ed.nnn.zane.nframe.data.NBGMedia
+import nico.ed.nnn.zane.nframe.data.NFrameHeader
+import nico.ed.nnn.zane.nframe.data.NFrameHeaderCard
+import nico.ed.nnn.zane.nframe.ui.compose.organisms.NFrameExoPlayer
+
+@Composable
+fun NFrameBox(
+    nFrameHeader: NFrameHeader,
+    nBGMedia: NBGMedia,
+    nFrameHeaderCard: NFrameHeaderCard
+) {
+    if (nFrameHeader == NFrameHeader.PRESENT) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(
+                    if (nBGMedia == NBGMedia.SOLID) {
+                        Color.Gray
+                    } else {
+                        Color.Transparent
+                    }
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            when (nBGMedia) {
+                NBGMedia.MOVIE -> {
+                    NFrameExoPlayer(
+                        modifier = Modifier,
+                        uri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                    )
+                }
+                NBGMedia.IMAGE -> Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.sample),
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentDescription = null
+                    )
+                    if (nFrameHeaderCard == NFrameHeaderCard.PRESENT) {
+                        Box(
+                            modifier = Modifier
+                                .width(168.dp)
+                                .height(100.dp)
+                                .background(color = Color.Gray)
+                        )
+                    }
+                }
+                else -> {}
+            }
+            if (nFrameHeaderCard == NFrameHeaderCard.PRESENT) {
+                Box(
+                    modifier = Modifier
+                        .width(168.dp)
+                        .height(100.dp)
+                        .background(color = Color.Gray)
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/organisms/NFrameDisplay.kt
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/organisms/NFrameDisplay.kt
@@ -1,23 +1,19 @@
 package nico.ed.nnn.zane.nframe.ui.compose.organisms
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import nico.ed.nnn.zane.nframe.R
 import nico.ed.nnn.zane.nframe.data.NBGMedia
 import nico.ed.nnn.zane.nframe.data.NFrameHeader
 import nico.ed.nnn.zane.nframe.data.NFrameHeaderCard
 import nico.ed.nnn.zane.nframe.data.NFrameHeaderFixed
+import nico.ed.nnn.zane.nframe.ui.compose.atoms.NFrameBox
 
 @Composable
 fun NFrameDisplay(
@@ -27,74 +23,31 @@ fun NFrameDisplay(
     nFrameHeaderFixed: NFrameHeaderFixed,
     content: List<String>
 ) {
-    val modifier = if (nFrameHeaderFixed == NFrameHeaderFixed.FIXED) {
-        Modifier.verticalScroll(rememberScrollState())
-    } else {
-        Modifier
-    }
-    val lazyModifier = if (nFrameHeaderFixed == NFrameHeaderFixed.FIXED) {
-        Modifier
-    } else {
-        Modifier.verticalScroll(rememberScrollState())
-    }
-    Column(modifier = modifier) {
-        if (nFrameHeader == NFrameHeader.PRESENT) {
-            when (nBGMedia) {
-                NBGMedia.MOVIE -> {}
-//                    NFrameExoPlayer(
-//                    modifier = Modifier,
-//                    uri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-//                )
-                NBGMedia.IMAGE -> Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.sample),
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        contentDescription = null
+    Column {
+        if (nFrameHeaderFixed == NFrameHeaderFixed.FIXED) {
+            NFrameBox(
+                nFrameHeader,
+                nBGMedia,
+                nFrameHeaderCard
+            )
+        }
+        LazyColumn {
+            item {
+                if (nFrameHeaderFixed == NFrameHeaderFixed.MOVE) {
+                    NFrameBox(
+                        nFrameHeader,
+                        nBGMedia,
+                        nFrameHeaderCard
                     )
-                    if (nFrameHeaderCard == NFrameHeaderCard.PRESENT) {
-                        Box(
-                            modifier = Modifier
-                                .width(168.dp)
-                                .height(100.dp)
-                        )
-                    }
-                }
-                else -> Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .background(
-                            if (nBGMedia == NBGMedia.SOLID) {
-                                Color.Gray
-                            } else {
-                                Color.White
-                            }
-                        ),
-                    contentAlignment = Alignment.Center
-                ) {
-                    if (nFrameHeaderCard == NFrameHeaderCard.PRESENT) {
-                        Box(
-                            modifier = Modifier
-                                .width(168.dp)
-                                .height(100.dp)
-                        )
-                    }
                 }
             }
-        }
-        LazyColumn(modifier = lazyModifier) {
-            item(content) {
+            items(content) {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(start = 16.dp, end = 16.dp, top = 20.dp)
                         .height(100.dp)
+                        .background(color = Color.Gray),
                 )
             }
         }
@@ -106,9 +59,9 @@ fun NFrameDisplay(
 private fun PreviewNFrameDisplay() {
     NFrameDisplay(
         nFrameHeader = NFrameHeader.PRESENT,
-        nBGMedia = NBGMedia.IMAGE,
+        nBGMedia = NBGMedia.MOVIE,
         nFrameHeaderCard = NFrameHeaderCard.PRESENT,
-        nFrameHeaderFixed = NFrameHeaderFixed.FIXED,
-        content = listOf("test", "test", "test", "test")
+        nFrameHeaderFixed = NFrameHeaderFixed.MOVE,
+        content = listOf("test", "test1", "test2", "test3")
     )
 }
