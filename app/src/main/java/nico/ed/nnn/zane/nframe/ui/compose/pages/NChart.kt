8f93ea8193c7df2413092fc29ad3410881db838f
Mithrandir815 <takayuki_iida@dwango.co.jp>
2023-09-08 15:51:32 +0900

    vicoのアップデート

--- app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NChart.kt
+++ app/src/main/java/nico/ed/nnn/zane/nframe/ui/compose/pages/NChart.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -22,60 +26,88 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nico.ed.nnn.zane.nframe.R
-import nico.ed.nnn.zane.nframe.ui.compose.atoms.NFrameTopAppBar
 import nico.ed.nnn.zane.nframe.ui.compose.organisms.VerticalBarGraphScreen
+import nico.ed.nnn.zane.nframe.ui.theme.Gray200
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun NChart() {
-    Scaffold(
-        topBar = {
-            NFrameTopAppBar(R.string.charts)
-        }
-    ) { padding ->
-        Box(
+    Scaffold(modifier = Modifier) { padding ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(height = 270.dp)
+                .background(color = Gray200)
+                .verticalScroll(rememberScrollState())
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.nbg_media),
-                contentScale = ContentScale.Crop,
+            Box(
                 modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .height(140.dp)
-                    .fillMaxWidth(),
-                contentDescription = null,
-            )
-            Row(
-                modifier = Modifier.padding(top = 28.dp, bottom = 24.dp, start = 16.dp)
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max)
             ) {
-                Box(
+                Image(
+                    painter = painterResource(id = R.drawable.nbg_media_png),
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .size(80.dp)
-                        .background(color = Color.White, shape = RoundedCornerShape(40.dp))
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    contentDescription = null,
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                Column(modifier = Modifier.padding(start = 16.dp)) {
-                    Text(
-                        text = "山田　太郎",
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
+                Row(
+                    modifier = Modifier
+                        .padding(top = 28.dp, bottom = 48.dp, start = 16.dp)
+                        .height(IntrinsicSize.Max)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .background(color = Color.White, shape = RoundedCornerShape(40.dp))
                     )
-                    Text(
-                        modifier = Modifier.padding(top = 8.dp),
-                        text = "N予備校ID 594856",
-                        color = Color.White
-                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Column(modifier = Modifier.padding(start = 16.dp)) {
+                        Text(
+                            modifier = Modifier.padding(end = 16.dp),
+                            text = "山田　太郎太郎太郎太郎太郎太郎太郎太郎太郎太郎太郎太郎",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                        Text(
+                            modifier = Modifier.padding(top = 8.dp),
+                            text = "N予備校ID 594856",
+                            color = Color.White
+                        )
+                    }
                 }
             }
-            VerticalBarGraphScreen(modifier = Modifier.align(Alignment.BottomCenter))
+            VerticalBarGraphScreen(modifier = Modifier.offset(y = (-32).dp))
+            Column(
+                modifier = Modifier
+                    .offset(y = (-16).dp)
+                    .background(color = Color.White)
+            ) {
+                repeat(30) {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(color = Color.Black)
+                        )
+                        Text(
+                            modifier = Modifier.padding(16.dp),
+                            text = "テスト",
+                            color = Color.Black,
+                            fontSize = 20.sp
+                        )
+
+                    }
+                }
+            }
         }
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 private fun NChartSample(modifier: Modifier = Modifier) {
     NChart()
